c02f49abd820c1d54e579be0f0a1c445f4ef7148
Michal Procházka <michal@prochazkaml.eu>
2025-06-07 21:50:59 +0200

    Implemented flat head screws

--- src/components/screw.py
+++ src/components/screw.py
@@ -3,12 +3,16 @@ from src.components.component import BasicComponent
 from reportlab.pdfgen.canvas import Canvas
 
 class Screw(BasicComponent):
-    def __init__(self, name: str, a: str, h: str, l: str):
+    def __init__(self, name: str, a: str, h: str, l: str = None):
         self.value = name
         self.type = "screw"
         self.str1 = "a = {}".format(a)
         self.str2 = "h = {}".format(h)
-        self.str3 = "l = {}".format(l)
+
+        if l != None:
+            self.str3 = "l = {}".format(l)
+        else:
+            self.str3 = None
 
     def draw_screw_thread(self, c: Canvas, x: float, y: float, r: float, h: float) -> None:
         c.line(x - r, y, x + r, y)
@@ -35,6 +39,15 @@ class RoundHeadScrew(Screw):
         c.line(x + size, y, x + size, y + size / 2)
 
         c.arc(x - size, y, x + size, y + size, 0, 180)
+
+        self.draw_screw_thread(c, x, y, size / 2, size)
+
+class FlatHeadScrew(Screw):
+    def draw_icon(self, c: Canvas, x: float, y: float, size: float) -> None:
+        c.line(x - size, y, x + size, y)
+        c.line(x - size, y, x - size, y + size)
+        c.line(x + size, y, x + size, y + size)
+        c.line(x - size, y + size, x + size, y + size)
 
         self.draw_screw_thread(c, x, y, size / 2, size)
 
